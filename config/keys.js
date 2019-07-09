bab4ad8bc3d9cca1a7735aa9650e546bc7f7d5fd
Uday Singh <uday144@gmail.com>
2019-07-09 16:09:04 +0530

    Add mongoDB connection

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://uday:<April@2001>@cluster1-9begx.mongodb.net/test?retryWrites=true&w=majority"
+};
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
 const app = express();
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
 
