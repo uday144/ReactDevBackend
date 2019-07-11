d59d12cf336d434f6985adaf5ed0b0d3af47d4e4
Uday <uday144@gmail.com>
2019-07-11 21:16:51 +0530

    completed Profile routes

--- validation/register.js
+++ validation/register.js
@@ -20,7 +20,7 @@ module.exports = function validateRegisterInput(data) {
     errors.email = "Email field is required";
   }
   if (!validator.isEmail(data.email)) {
-    errors.name = "Email is invalid";
+    errors.email = "Email is invalid";
   }
   if (validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
